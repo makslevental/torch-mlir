79a4d148425557ad0bc0d12504dc08fd8ee7ee75
Maksim <maksim.levental@gmail.com>
2021-12-25 03:21:27 -0600

    cleaned up

--- hls/example/make_matmul.sh
+++ hls/example/make_matmul.sh
@@ -1,3 +1,3 @@
-export PYTHONPATH=`pwd`/../build/tools/torch-mlir/python_packages/torch_mlir
-source ../mlir_venv/bin/activate
+export PYTHONPATH=`pwd`/../../build/tools/torch-mlir/python_packages/torch_mlir
+source ../../mlir_venv/bin/activate
 python poc.py
