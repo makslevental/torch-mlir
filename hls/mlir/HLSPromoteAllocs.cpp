7477e9ebf4125759c66848c2aaf2d72e6c3db0c3
Maksim <maksim.levental@gmail.com>
2022-01-04 08:13:30 -0600

    memory planning for braggnn

--- hls/mlir/HLSPromoteAllocs.cpp
+++ hls/mlir/HLSPromoteAllocs.cpp
@@ -9,20 +9,20 @@
 
 #include "HLSPassDetail.h"
 #include "HLSPasses.h"
+#include "MemoryPlanning.h"
+#include "mlir/Analysis/SliceAnalysis.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinOps.h"
-#include "mlir/Analysis/SliceAnalysis.h"
 #include "mlir/Pass/Pass.h"
+#include <iostream>
 
 using namespace mlir;
 using namespace mlir::torch::HLS;
 
-
-
-using namespace mlir;
+using LiveRanges = DenseMap<Operation *, llvm::SmallVector<size_t>>;
 
 template <typename AnyMemRefDefOp>
 static bool isMemRefSizeValidSymbol(AnyMemRefDefOp memrefDefOp, unsigned index,
@@ -37,18 +37,61 @@ static bool isMemRefSizeValidSymbol(AnyMemRefDefOp memrefDefOp, unsigned index,
                        region);
 }
 
+template <class Container> int product(const Container &container) {
+  auto i = container.begin();
+  int prod = 1;
+
+  while (i != container.end())
+    prod *= *i++;
+
+  return prod;
+}
+
 namespace {
 
 class PromoteAllocsPass : public HLSPromoteAllocsBase<PromoteAllocsPass> {
+
   void runOnOperation() override {
     auto module = getOperation();
     module.walk([&](FuncOp func) {
-      auto sortedOps = topoSort(func);
-      hoistAllocs(func, sortedOps);
+      LiveRanges liveRanges_ = liveRanges(func);
+      SortedUniqueLiveRangeMap<size_t> sortedLiveRanges;
+      for (const auto &item : liveRanges_) {
+        auto op = llvm::cast<memref::AllocOp>(item.getFirst());
+        auto lvr = item.getSecond();
+        MemRefType type = op.getType();
+        sortedLiveRanges.insert(
+            {{{lvr[0], lvr[1]}, op, ""}, product(getConcreteShape(op, func))});
+      }
+      std::vector<PlannedAlloc> plannedAllocs =
+          greedyBySizeWithSmallestGap(sortedLiveRanges);
+      hoistAllocs(func, plannedAllocs);
     });
   }
 
-  SetVector<Operation *> topoSort(FuncOp func) {
+  std::vector<int64_t> getConcreteShape(memref::AllocOp op, FuncOp func) {
+    MemRefType type = op.getType();
+    std::vector<int64_t> shape;
+    auto dynamicSizes = op.getDynamicSizes();
+    for (int idx = 0; idx < type.getShape().size(); ++idx) {
+      auto dimSize = type.getShape()[idx];
+      if (dimSize < 0) {
+        if (isMemRefSizeValidSymbol(op, idx, func->getParentRegion())) {
+          unsigned dynamicDimPos = type.getDynamicDimIndex(idx);
+          dimSize = dynamicSizes[dynamicDimPos]
+                        .getDefiningOp<arith::ConstantIndexOp>()
+                        .value();
+        } else {
+          shape.push_back(-1);
+          continue;
+        }
+      }
+      shape.push_back(dimSize);
+    }
+    return shape;
+  }
+
+  LiveRanges liveRanges(FuncOp func) {
     SetVector<Operation *> sortedOps;
     func.walk([&sortedOps, &func](Operation *op) {
       if (op->getName() == func->getName())
@@ -60,67 +103,62 @@ class PromoteAllocsPass : public HLSPromoteAllocsBase<PromoteAllocsPass> {
       return WalkResult::advance();
     });
     sortedOps = topologicalSort(sortedOps);
-    // not sure why but above is reverse toposort
-    SetVector<Operation *> reversedSortedOps(sortedOps.rbegin(), sortedOps.rend());
-    return reversedSortedOps;
+    DenseMap<Operation *, size_t> opToTopoIndex;
+    for (int i = 0; i < sortedOps.size(); ++i) {
+      opToTopoIndex[sortedOps[i]] = i;
+    }
+    LiveRanges allocLiveRange;
+    for (const auto &item : opToTopoIndex) {
+      auto op = item.first;
+      if (!llvm::isa<memref::AllocOp>(op))
+        continue;
+      for (Operation *user : op->getUsers()) {
+        auto idx = opToTopoIndex.find(user)->getSecond();
+        allocLiveRange[op].push_back(idx);
+      }
+    }
+    for (auto &item : allocLiveRange) {
+      size_t start = *std::min_element(item.second.begin(), item.second.end());
+      size_t end = *std::max_element(item.second.begin(), item.second.end());
+      llvm::SmallVector<size_t> liveRange{start, end};
+      allocLiveRange[item.first] = liveRange;
+    }
+
+    return allocLiveRange;
   }
 
-  void hoistAllocs(FuncOp func, SetVector<Operation *> sortedOps) {
+  void hoistAllocs(FuncOp func, std::vector<PlannedAlloc> plannedAllocs) {
+
     auto inputSlab =
         MemRefType::get({1000000}, IntegerType::get(func.getContext(), 8));
     func.insertArgument(func.getNumArguments(), inputSlab, {});
     BlockArgument slabArg = func.getArgument(func.getNumArguments() - 1);
     OpBuilder builder(func.getBody());
 
-//    auto builder = OpBuilder::atBlockBegin(&func.getBody().front());
-//    auto zero = builder.createOrFold<arith::ConstantIndexOp>(func.getBody().getLoc(), 0L);
-    auto maxSize = 0;
-    auto offset = 0;
-
-    for (auto &_op : sortedOps) {
-      if (!llvm::isa<memref::AllocOp>(_op))
-        continue;
-
-      auto op = llvm::cast<memref::AllocOp>(_op);
+    size_t maxSize = 0;
+    for (const auto &pAlloc : plannedAllocs) {
+      auto op = llvm::cast<memref::AllocOp>(pAlloc.ulvr.id);
       MemRefType type = op.getType();
-      std::vector<int64_t> shape;
-      auto dynamicSizes = op.getDynamicSizes();
-
-      auto size = 1;
-      for (int idx = 0; idx < type.getShape().size(); ++idx) {
-        auto dimSize = type.getShape()[idx];
-        if (dimSize < 0) {
-          if (isMemRefSizeValidSymbol(op, idx, func->getParentRegion())) {
-            unsigned dynamicDimPos = type.getDynamicDimIndex(idx);
-            dimSize = dynamicSizes[dynamicDimPos]
-                .getDefiningOp<arith::ConstantIndexOp>()
-                .value();
-          } else {
-            shape.push_back(-1);
-            continue;
-          }
-        }
-        size *= dimSize;
-        shape.push_back(dimSize);
-      }
-      maxSize = std::max(maxSize, size);
+      auto shape = getConcreteShape(op, func);
       auto promotedType =
           MemRefType::get(shape, type.getElementType(), type.getLayout(),
                           type.getMemorySpace());
 
       builder.setInsertionPoint(op);
-      auto offsetConstant = builder.create<arith::ConstantIndexOp>(op->getLoc(), offset);
-      offset += size * 4;
+      auto offsetConstant = builder.create<arith::ConstantIndexOp>(
+          op->getLoc(), pAlloc.reg.offset);
+      maxSize = std::max(maxSize, pAlloc.reg.nextOffset());
       SmallVector<Value, 4> newOperands;
       builder.setInsertionPoint(op);
-      Value view = builder.create<memref::ViewOp>(op->getLoc(),
-                                                  MemRefType::get(shape, type.getElementType()), slabArg, offsetConstant,
-                                                  newOperands);
+      Value view = builder.create<memref::ViewOp>(
+          op->getLoc(), MemRefType::get(shape, type.getElementType()), slabArg,
+          offsetConstant, newOperands);
       op.replaceAllUsesWith(view);
     }
 
     auto correctInputSlab =
-        MemRefType::get({offset}, IntegerType::get(func.getContext(), 8));
+        MemRefType::get({static_cast<long>(maxSize * 4)},
+                        IntegerType::get(func.getContext(), 8));
     func.insertArgument(func.getNumArguments(), correctInputSlab, {});
     BlockArgument correctSlabArg = func.getArgument(func.getNumArguments() - 1);
     slabArg.replaceAllUsesWith(correctSlabArg);
