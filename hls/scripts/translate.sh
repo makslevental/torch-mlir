0bfd8a72a57aa1e09e5c57d497d0a4f2d63209e0
maksim <maksim.levental@gmail.com>
2022-04-10 17:05:18 -0500

    eliminate stores loads

--- hls/scripts/translate.sh
+++ hls/scripts/translate.sh
@@ -1,5 +1,5 @@
 set -e
-torch-mlir-opt forward.mlir -torch-hls-promote-allocs="hoist-globals" -cse -symbol-dce -o forward.mlir.promoted
+torch-mlir-opt forward.mlir -torch-hls-promote-allocs -cse -symbol-dce -o forward.mlir.promoted
 scalehls-translate forward.mlir.promoted --emit-hlspy --mlir-print-elementsattrs-with-hex-if-larger=-1 -o forward.py
 black forward.py
 python mlir_ops.py
