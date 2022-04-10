45c11bfb6bf8110b451f0068bf8030df8fe9fa53
maksim <maksim.levental@gmail.com>
2022-04-09 20:30:07 -0500

    .

--- hls/scripts/translate.sh
+++ hls/scripts/translate.sh
@@ -1,4 +1,6 @@
-scalehls-translate forward.mlir --emit-hlspy --mlir-print-elementsattrs-with-hex-if-larger=-1 -o forward.py
+set -e
+torch-mlir-opt forward.mlir -torch-hls-promote-allocs="hoist-globals" -cse -symbol-dce -o forward.mlir.promoted
+scalehls-translate forward.mlir.promoted --emit-hlspy --mlir-print-elementsattrs-with-hex-if-larger=-1 -o forward.py
 black forward.py
 python mlir_ops.py
 python forward_rewritten.py > forward.ll
