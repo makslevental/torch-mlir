a7f446d4ec705690ee202b7cd2028f10fc619cad
max <maksim.levental@gmail.com>
2022-04-15 00:26:26 -0500

    artisanal RTL

--- hls/scripts/run_vivado.sh
+++ hls/scripts/run_vivado.sh
@@ -1,7 +1,6 @@
 set -e
 #FN=forward.mlir make
-cp hls_hooks.tcl /home/$USER/dev_projects/Xilinx/Vitis_HLS/2021.2/common/scripts/hls_hooks.tcl
 source /home/$USER/dev_projects/Xilinx/Vitis_HLS/2021.2/settings64.sh
-vitis_hls run_hls.tcl
+vivado -mode tcl -stack 2000 -source run_vivado.tcl
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 cp -R proj "proj-$timestamp"
