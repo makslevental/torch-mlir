8e5a5e5943da35edbc9eb5e77af8785e02330d56
max <maksim.levental@gmail.com>
2022-05-06 19:55:43 -0500

    a bunch of catch up

--- hls/mlir/HLSPasses.cpp
+++ hls/mlir/HLSPasses.cpp
@@ -1,5 +1,6 @@
 #include "HLSPasses.h"
 #include "mlir/IR/BuiltinOps.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/InitAllPasses.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Transforms/Passes.h"
@@ -38,7 +39,7 @@ void mlir::torch::HLS::registerHLSTorchPasses() {
       "torch-function-to-torch-hls-backend-pipeline", "",
       mlir::torch::HLS::createTorchScriptFunctionToTorchHLSBackendPipeline);
 
-  registerDirtyPass();
+//  registerDirtyPass();
 }
 
 void mlir::torch::HLS::createTorchScriptModuleToTorchHLSBackendPipeline(
@@ -60,33 +61,33 @@ void createTorchScriptFunctionToTorchHLSBackendPipeline(OpPassManager &pm) {
   pm.addPass(createAdjustCallingConventionsPass());
 
   if (true) {
-    pm.addNestedPass<FuncOp>(createCanonicalizerPass());
+    pm.addNestedPass<func::FuncOp>(createCanonicalizerPass());
     pm.addPass(createInlineGlobalSlotsPass());
   }
 
-  pm.addNestedPass<FuncOp>(createReduceOpVariantsPass());
+  pm.addNestedPass<func::FuncOp>(createReduceOpVariantsPass());
 
   if (true) {
-    pm.addNestedPass<FuncOp>(createCanonicalizerPass());
+    pm.addNestedPass<func::FuncOp>(createCanonicalizerPass());
     pm.addPass(createSymbolDCEPass());
   }
 
   // ours
-  pm.addNestedPass<FuncOp>(HLS::createHLSRefineTypesPass());
+  pm.addNestedPass<func::FuncOp>(HLS::createHLSRefineTypesPass());
 
   pm.addPass(Torch::createRefinePublicReturnPass());
 
   if (true) {
-    pm.addNestedPass<FuncOp>(createCanonicalizerPass());
+    pm.addNestedPass<func::FuncOp>(createCanonicalizerPass());
   }
-  pm.addNestedPass<FuncOp>(Torch::createMaximizeValueSemanticsPass());
+  pm.addNestedPass<func::FuncOp>(Torch::createMaximizeValueSemanticsPass());
 
   if (true) {
-    pm.addNestedPass<FuncOp>(createCanonicalizerPass());
+    pm.addNestedPass<func::FuncOp>(createCanonicalizerPass());
   }
   //  pm.addPass(HLS::createHLSDropPublicReturnPass());
-  pm.addNestedPass<FuncOp>(Torch::createDecomposeComplexOpsPass());
-  pm.addNestedPass<FuncOp>(HLS::createHLSDecomposeComplexOpsPass());
+  pm.addNestedPass<func::FuncOp>(Torch::createDecomposeComplexOpsPass());
+  pm.addNestedPass<func::FuncOp>(HLS::createHLSDecomposeComplexOpsPass());
 
   // TODO: VerifyHLSTorchBackendContractPass.
 }
@@ -99,7 +100,7 @@ void registerHLSConversionPasses() {
 }
 
 void createTorchBackendToLinalgOnTensorsBackendPipeline(OpPassManager &pm) {
-  //  pm.addNestedPass<FuncOp>(createHLSConvertOperatorsPass());
+  //  pm.addNestedPass<func::FuncOp>(createHLSConvertOperatorsPass());
   // Check some invariants to catch errors in a clear way.
   pm.addPass(
       TorchConversion::createVerifyInvariantsBeforeBackendLoweringPass());
@@ -108,26 +109,26 @@ void createTorchBackendToLinalgOnTensorsBackendPipeline(OpPassManager &pm) {
   // We do this first as it tends to involve pattern-matching against constants,
   // (e.g. dimensions which must be constant in a ranked programming model)
   // and those constants get somewhat obscured by TorchToStd.
-  pm.addNestedPass<FuncOp>(createHLSConvertTorchToLinalgPass());
-  pm.addNestedPass<FuncOp>(createConvertTorchToLinalgPass());
-  pm.addNestedPass<FuncOp>(createConvertTorchToStdPass());
-  pm.addNestedPass<FuncOp>(createConvertTorchToSCFPass());
-  pm.addNestedPass<FuncOp>(memref::createExpandOpsPass());
+  pm.addNestedPass<func::FuncOp>(createHLSConvertTorchToLinalgPass());
+  pm.addNestedPass<func::FuncOp>(createConvertTorchToLinalgPass());
+  pm.addNestedPass<func::FuncOp>(createConvertTorchToStdPass());
+  pm.addNestedPass<func::FuncOp>(createConvertTorchToSCFPass());
+  pm.addNestedPass<func::FuncOp>(memref::createExpandOpsPass());
 
   if (true) {
     // Clean up any non-canonical code introduced above..
-    pm.addNestedPass<FuncOp>(createCanonicalizerPass());
+    pm.addNestedPass<func::FuncOp>(createCanonicalizerPass());
     // Resolve `dim` ops on tensors (which currently live in the `memref`
     // dialect for some reason -- we don't have memrefs at this level).
-    pm.addNestedPass<FuncOp>(memref::createResolveShapedTypeResultDimsPass());
+    pm.addNestedPass<func::FuncOp>(memref::createResolveShapedTypeResultDimsPass());
     // The resolution of `dim` ops tends to create identical ops. CSE them.
-    pm.addNestedPass<FuncOp>(createCSEPass());
+    pm.addNestedPass<func::FuncOp>(createCSEPass());
   }
 
   // Finish the type conversion from `torch` types to the types of the
   // linalg-on-tensors backend contract.
   pm.addPass(TorchConversion::createFuncBackendTypeConversionPass());
-  pm.addNestedPass<FuncOp>(
+  pm.addNestedPass<func::FuncOp>(
       TorchConversion::createFinalizingBackendTypeConversionPass());
 
   // Verify that we have lowered to the form that linalg on tensors backends
