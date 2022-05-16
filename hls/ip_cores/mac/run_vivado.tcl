d06c5557ab076f6468dd9834d1a582a859199091
max <maksim.levental@gmail.com>
2022-05-16 13:31:07 -0500

    bram weights and inputs

--- hls/ip_cores/mac/run_vivado.tcl
+++ hls/ip_cores/mac/run_vivado.tcl
@@ -4,26 +4,17 @@ set_property board_part xilinx.com:au280:part0:1.1 [current_project]
 add_files -fileset constrs_1 -norecurse alveo-u280-xdc.xdc
 import_files -fileset constrs_1 alveo-u280-xdc.xdc
 
+
 create_ip -name floating_point -vendor xilinx.com -library ip -version 7.1 -module_name accum_half_precision
 source half_precision_accum.tcl
 generate_target {instantiation_template} [get_files project_1/project_1.srcs/sources_1/ip/accum_half_precision/accum_half_precision.xci]
 update_compile_order -fileset sources_1
 generate_target all [get_files  project_1/project_1.srcs/sources_1/ip/accum_half_precision/accum_half_precision.xci]
 
-create_ip -name floating_point -vendor xilinx.com -library ip -version 7.1 -module_name mul_half_precision
-source half_precision_mul.tcl
-generate_target {instantiation_template} [get_files project_1/project_1.srcs/sources_1/ip/mul_half_precision/mul_half_precision.xci]
 update_compile_order -fileset sources_1
-generate_target all [get_files  project_1/project_1.srcs/sources_1/ip/mul_half_precision/mul_half_precision.xci]
 
-add_files -norecurse -scan_for_includes mac.v -force
-import_files -norecurse mac.v -force
-
-add_files -norecurse -scan_for_includes relu.v -force
-import_files -norecurse relu.v -force
-
-add_files -norecurse -scan_for_includes forward.v -force
-import_files -norecurse forward.v -force
+add_files -norecurse -scan_for_includes out.v -force
+import_files -norecurse out.v -force
 
 update_compile_order -fileset sources_1
 
@@ -36,12 +27,24 @@ set_property STEPS.SYNTH_DESIGN.ARGS.RESOURCE_SHARING on [get_runs synth_1]
 launch_runs synth_1 -jobs 16
 wait_on_run synth_1
 open_run synth_1
-#opt_design
+
+report_utilization -file syn_before_opt_util.rpt
+report_utilization -file syn_before_opt_hier_util.rpt -hierarchical -hierarchical_percentages
+report_timing_summary -delay_type max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -file syn_before_opt_timing.rpt
+report_power -file syn_before_opt_power.rpt
+
+opt_design
+
+report_utilization -file syn_after_opt_util.rpt
+report_utilization -file syn_after_opt_hier_util.rpt -hierarchical -hierarchical_percentages
+report_timing_summary -delay_type max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -file syn_after_opt_timing.rpt
+report_power -file syn_after_opt_power.rpt
+
 launch_runs impl_1 -jobs 16
-report_utilization -file project_1/hier_util.rpt -hierarchical -hierarchical_percentages
-create_clock -period 10.000 -name clk -waveform {0.000 5.000} [get_ports ap_clk]
-report_timing -file project_1/timing_synth.rpt
-report_timing_summary -file project_1/timing_synth_summary.rpt
+wait_on_run impl_1
+open_run impl_1
 
-
-
+report_utilization -file impl_after_opt_util.rpt
+report_utilization -file impl_after_opt_hier_util.rpt -hierarchical -hierarchical_percentages
+report_timing_summary -delay_type max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -file impl_after_opt_timing.rpt
+report_power -file impl_after_opt_power.rpt
