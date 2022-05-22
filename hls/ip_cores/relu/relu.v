23beade59ae95c8edecdb102ce87a5d3aa4af15f
max <maksim.levental@gmail.com>
2022-05-22 18:11:31 -0500

    handgen v2

--- hls/ip_cores/relu/relu.v
+++ hls/ip_cores/relu/relu.v
@@ -1,7 +1,11 @@
-`timescale 1ns / 1ps
-module relu(
-    input [31:0] din_relu,
-    output [31:0] dout_relu
-  );
-  assign dout_relu = (din_relu[31] == 0)? din_relu : 0;   //if the sign bit is high, send zero on the output else send the input
+`timescale 1ns/1ps
+module relu
+    #(parameter
+        ID=1,
+        WIDTH=16
+    )(
+    input[WIDTH-1:0] a,
+    output[WIDTH-1:0] z
+);
+    assign z = (a[WIDTH-1] == 0) ? a:0;   //if the sign bit is high, send zero on the output else send the input
 endmodule
--- hls/scripts/run_vivado.tcl
+++ hls/scripts/run_vivado.tcl
@@ -1,19 +1,27 @@
-create_project braggnn_vivado vivado -part xcu280-fsvh2892-2L-e -force
+# BEGIN Vivado Commands
+set vivado_ver [version -short]
+set fpo_ver 7.1
+if {[regexp -nocase {2015\.1.*} \
+ $vivado_ver match]} \
+ {
+    set fpo_ver 7.0
+} \
+
+create_project project_1 project_1 -part xcu280-fsvh2892-2L-e -force
 set_property board_part xilinx.com:au280:part0:1.1 [current_project]
-create_clock -name SysClk -period 10 -waveform {0 5} [get_ports clk]
 
 add_files -fileset constrs_1 -norecurse alveo-u280-xdc.xdc
 import_files -fileset constrs_1 alveo-u280-xdc.xdc
 
-add_files -scan_for_includes {forward_fmuladd_32ns_32ns_32_10_med_dsp_1.v}
-import_files {forward_fmuladd_32ns_32ns_32_10_med_dsp_1.v}
-source forward_fmuladd_32ns_32ns_32_10_med_dsp_1_ip.tcl
-
-update_compile_order -fileset sources_1
-
 add_files -norecurse -scan_for_includes forward.v -force
 import_files -norecurse forward.v -force
 
+source half_fadd.tcl
+source half_fmul.tcl
+
+update_compile_order -fileset sources_1
+
+set_property top forward [current_fileset]
 set_property strategy Flow_AreaOptimized_high [get_runs synth_1]
 set_property STEPS.SYNTH_DESIGN.ARGS.RETIMING true [get_runs synth_1]
 set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY full [get_runs synth_1]
@@ -22,13 +30,27 @@ set_property STEPS.SYNTH_DESIGN.ARGS.RESOURCE_SHARING on [get_runs synth_1]
 launch_runs synth_1 -jobs 16
 wait_on_run synth_1
 open_run synth_1
+
+create_clock -name clk -period 10 -waveform {0 5} [get_ports clock]
+
+file mkdir reports
+report_utilization -file reports/syn_before_opt_util.rpt
+report_utilization -file reports/syn_before_opt_hier_util.rpt -hierarchical -hierarchical_percentages
+report_timing_summary -file reports/syn_before_opt_timing.rpt -delay_type max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins
+report_power -file reports/syn_before_opt_power.rpt
+
 opt_design
-report_utilization -file hier_util.rpt -hierarchical -hierarchical_percentages
-report_timing_summary -file timing_synth.log
 
+report_utilization -file reports/syn_after_opt_util.rpt
+report_utilization -file reports/syn_after_opt_hier_util.rpt -hierarchical -hierarchical_percentages
+report_timing_summary -file reports/syn_after_opt_timing.rpt -delay_type max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins
+report_power -file reports/syn_after_opt_power.rpt
 
-foreach IPfile [get_ips] {
-    puts "\n $IPfile: \n";
-    foreach prop [list_property [get_ips $IPfile] -regexp {^CONFIG\.\w+$}] {
-        puts "$prop \{[get_property $prop [get_ips $IPfile]]\}";
-}}
+launch_runs impl_1 -jobs 16
+wait_on_run impl_1
+open_run impl_1
+
+report_utilization -file reports/impl_after_opt_util.rpt
+report_utilization -file reports/impl_after_opt_hier_util.rpt -hierarchical -hierarchical_percentages
+report_timing_summary -file reports/impl_after_opt_timing.rpt -delay_type max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins
+report_power -file reports/impl_after_opt_power.rpt
