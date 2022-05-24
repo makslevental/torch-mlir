ac637c83d50920dcfd19f74f9ae8b82019483375
max <maksim.levental@gmail.com>
2022-05-23 21:36:07 -0500

    graph traversal

--- hls/scripts/llvm_val.py
+++ hls/scripts/llvm_val.py
@@ -1,14 +1,26 @@
+from __future__ import annotations
+
 import io
 import itertools
+import re
+import struct
 import sys
 from collections import defaultdict, deque
+from typing import Tuple
 
+import networkx as nx
 import numpy as np
+
+from hls.scripts.mlir_ops import get_default_args, get_array_type, index_map
+
+
+def float_to_hex(f):
+    return hex(struct.unpack("<I", struct.pack("<f", f))[0])
 
 
 def format_cst(cst):
-    return cst
-    # return float(np.random.randint(1, 100000))
+    # return float_to_hex(float(cst))
+    return float(np.random.randint(1, 100000))
     # return cst
     # return (handleDoubleToHex(cst)[:11] + "0" * 7).upper().replace("X", "x")
 
@@ -16,6 +28,7 @@ def format_cst(cst):
 VAR_COUNT = 0
 
 FILE = open("forward.ll", "w")
+
 
 class LLVMVal:
     def __init__(self, name):
@@ -96,6 +109,87 @@ class LLVMConstant(LLVMVal):
         return self._fmt
 
 
+ArrayIndex = Tuple[int]
+
+
+class ArrayVal(LLVMVal):
+    array: ArrayDecl
+
+    def __init__(self, name, val_id: ArrayIndex, array: ArrayDecl):
+        super().__init__(name)
+        self.array = array
+        self.var_id = "_".join(map(str, val_id))
+
+    def __str__(self):
+        return f"%{self.name}_{self.var_id}"
+
+
+class GlobalArrayVal(ArrayVal):
+    def __str__(self):
+        return f"%{self.name}_{self.var_id}"
+
+
+class GlobalArray:
+    def __init__(self, name, global_name, global_array):
+        self.name = name
+        self.global_name = global_name
+        self.global_array = global_array
+        self.curr_shape = global_array.shape
+        self.csts = {}
+
+    def __getitem__(self, index: ArrayIndex):
+        if index not in self.csts:
+            v = GlobalArrayVal(self.global_name, index, self)
+            self.csts[index] = v
+            print(f"{v} = fptrunc double {format_cst('')} to float", file=FILE)
+        return self.csts[index]
+
+
+class ArrayDecl:
+    def __init__(self, arr_name, *shape, input=False, output=False):
+        self.arr_name = arr_name
+        self.curr_shape = shape
+        self.prev_shape = shape
+        self.pe_index = shape
+        self.registers = {}
+        self.input = input
+        self.output = output
+
+    def __getitem__(self, index: ArrayIndex):
+        global PES
+        try:
+            index = self.idx_map(index)
+        except ValueError:
+            index = (-1, -1, -1, -1)
+
+        if index not in self.registers:
+            if not self.input:
+                v = LLVMConstant("0.0")
+            else:
+                v = ArrayVal(f"{self.arr_name}", index, self)
+            self.registers[index] = v
+
+        v = self.registers[index]
+        return v
+
+    def __setitem__(self, index, value):
+        global PES
+        try:
+            index = self.idx_map(index)
+        except ValueError:
+            index = (-1, -1, -1, -1)
+        assert not self.input
+        self.registers[index] = value
+
+    def idx_map(self, index):
+        return index_map(index, self.curr_shape, self.prev_shape)
+
+    def reshape(self, *shape):
+        self.prev_shape = self.curr_shape
+        self.curr_shape = shape
+        return self
+
+
 def Exp(val):
     v = LLVMVal(f"(exp ({val})")
     print(f"{v} = call float @expf(float {val})", file=FILE)
@@ -124,7 +218,10 @@ class MMAC:
         if kwargs["type"] == "MulAdd":
             a, b, c = args
             v = LLVMVal(f"(mac ({a} {b} {c})")
-            print(f"{v} = call float @llvm.fmuladd.f32(float {a}, float {b}, float {c})", file=FILE)
+            print(
+                f"{v} = call float @llvm.fmuladd.f32(float {a}, float {b}, float {c})",
+                file=FILE,
+            )
             return v
         else:
             a, b = args
@@ -132,6 +229,7 @@ class MMAC:
                 return a + b
             elif kwargs["type"] == "Mult":
                 return a * b
+
 
 MACS = {}
 
@@ -161,7 +259,7 @@ class RReLU:
 
     def __call__(self, val):
         v = LLVMVal(f"(relu ({val})")
-        print(f"{v} = call float @expf(float {val})", file=FILE)
+        print(f"{v} = call float @relu(float {val})", file=FILE)
         return v
 
 
@@ -197,93 +295,145 @@ def FMulAdd(a, b, c):
 
 
 def make_args_globals(Args):
-    from mlir_ops import get_array_type
-    from mlir_ops import ArrayDecl, Global
-
     args = []
     globals = []
     for _arg_name, arg in Args.items():
         if isinstance(arg, ArrayDecl):
             typ = get_array_type(arg.curr_shape, nd=True, ptr=False)
-            args.append(f"{typ}* noalias %{arg.var_name}")
-            # if arg.input:
-            #     for index in np.ndindex(*arg.curr_shape):
-            # elif arg.output:
-            #     for index in np.ndindex(*arg.curr_shape):
-            #         args.append(f"float* %{arg.var_name}_{'_'.join(map(str, index))}")
-            # elif arg.globl:
-            #     typ = get_array_type(arg.curr_shape, ptr=False)
-            #     globals.append(
-            #         f"@{arg.var_name} = common global {typ} zeroinitializer, align 4"
-            #     )
-        elif isinstance(arg, Global):
-            globals.append(arg)
+            args.append(f"{typ}* noalias %{arg}")
 
     return args, globals
 
 
-def LLVMForward(Args, OUTPUT_ARRAYS, forward):
+def LLVMForward(input, output, forward, processing_elts, max_range):
     global FILE
-    from mlir_ops import get_array_type
-    from mlir_ops import ArrayDecl
-
-    args, globals = make_args_globals(Args)
-
     print('source_filename = "LLVMDialectModule"', file=FILE)
     print("declare float @expf(float)", file=FILE)
     print("declare void @_ssdm_op_SpecResource(...)", file=FILE)
-    # print("declare float @relu(float)", file=FILE)
+    print("declare float @relu(float)", file=FILE)
     print("declare float @llvm.fmuladd.f32(float %a, float %b, float %c)", file=FILE)
-    print(f"define void @forward({', '.join(args)}) {{\n", file=FILE)
-    for _arg_name, arg in Args.items():
-        if isinstance(arg, ArrayDecl):
-            typ = get_array_type(arg.curr_shape, nd=True, ptr=False)
-            # shape = arg.curr_shape if len(arg.curr_shape) > 1 else (arg.curr_shape,)
-            for idx in itertools.product(*[range(a) for a in arg.curr_shape]):
-                idx = list(map(str, idx))
-                print(
-                    f"%{arg.var_name}_{'_'.join(idx)}_gep = getelementptr {typ}, {typ}* %{arg.var_name}, i32 0, {', '.join([f'i32 {i}' for i in idx])}", file=FILE
-                    # f"%mac_{mac.id}_{i}_gep = getelementptr {typ}, {typ}* %mac_{mac.id}, i16 0, i16 {i}", file=OLD_FILE
-                )
-                if arg.input:
-                    print(
-                        # f"%glob_{glo.var_name}_idx_{'_'.join(idx)} = load float, float* getelementptr inbounds ({typ}, {typ}* %glob_{glo.var_name}, i64 0, {', '.join([f'i64 {i}' for i in idx])})"
-                        f"%{arg.var_name}_{'_'.join(idx)} = load float, float* %{arg.var_name}_{'_'.join(idx)}_gep", file=FILE
-                    )
+    inputs = [
+        input.arr_name + "_" + "_".join(map(str, idx))
+        for idx in np.ndindex(input.curr_shape)
+    ]
+    outputs = [
+        output.arr_name + "_" + "_".join(map(str, idx))
+        for idx in np.ndindex(output.curr_shape)
+    ]
+    print(
+        f"define void @forward({', '.join([f'float %{a}' for a in inputs] + [f'float* %{a}' for a in outputs])}) {{\n",
+        file=FILE,
+    )
+    # for _arg_name, arg in Args.items():
+    #     if isinstance(arg, ArrayDecl):
+    #         typ = get_array_type(arg.curr_shape, nd=True, ptr=False)
+    #         # shape = arg.curr_shape if len(arg.curr_shape) > 1 else (arg.curr_shape,)
+    #         for idx in itertools.product(*[range(a) for a in arg.curr_shape]):
+    #             idx = list(map(str, idx))
+    #             print(
+    #                 f"%{arg.var_name}_{'_'.join(idx)}_gep = getelementptr {typ}, {typ}* %{arg.var_name}, i32 0, {', '.join([f'i32 {i}' for i in idx])}", file=FILE
+    #                 # f"%mac_{mac.id}_{i}_gep = getelementptr {typ}, {typ}* %mac_{mac.id}, i16 0, i16 {i}", file=OLD_FILE
+    #             )
+    #             if arg.input:
+    #                 print(
+    #                     # f"%glob_{glo.var_name}_idx_{'_'.join(idx)} = load float, float* getelementptr inbounds ({typ}, {typ}* %glob_{glo.var_name}, i64 0, {', '.join([f'i64 {i}' for i in idx])})"
+    #                     f"%{arg.var_name}_{'_'.join(idx)} = load float, float* %{arg.var_name}_{'_'.join(idx)}_gep", file=FILE
+    #                 )
 
     OLD_FILE = FILE
     FILE = io.StringIO()
     forward()
 
-    for mac_idx, mac in MACS.items():
-        typ = get_array_type((len(mac.csts),))
-        print(f"%mac_{mac.id} = alloca [{typ}], align 512", file=OLD_FILE)
-        print(f"call void (...) @_ssdm_op_SpecResource([{typ}]* %mac_{mac.id}, i64 666, i64 22, i64 2)", file=OLD_FILE)
-
-    for mac_idx, mac in MACS.items():
-        typ = get_array_type((len(mac.csts),), nd=False, ptr=False)
-        for i, cst in enumerate(mac.csts):
-            print(
-                # f"%glob_{glo.var_name}_idx_{'_'.join(idx)} = load float, float* getelementptr inbounds ({typ}, {typ}* %glob_{glo.var_name}, i64 0, {', '.join([f'i64 {i}' for i in idx])})"
-                f"%mac_{mac.id}_{i}_gep = getelementptr [{typ}], [{typ}]* %mac_{mac.id}, i16 0, i16 {i}", file=OLD_FILE
-            )
-            print(
-                # f"%glob_{glo.var_name}_idx_{'_'.join(idx)} = load float, float* getelementptr inbounds ({typ}, {typ}* %glob_{glo.var_name}, i64 0, {', '.join([f'i64 {i}' for i in idx])})"
-                f"%{cst} = load float, float* %mac_{mac.id}_{i}_gep", file=OLD_FILE
-            )
-
     FILE.seek(0)
     OLD_FILE.write(FILE.read())
 
-    for arr in OUTPUT_ARRAYS:
-        for index, value in arr.registers.items():
-            var_name = f"%{arr.var_name}_{'_'.join(map(str, index))}"
-            print(f"store float {value}, float* {var_name}_gep, align 4", file=OLD_FILE)
+    for idx, reg in output.registers.items():
+        id = "_".join(map(str, idx))
+        print(f"store float {reg}, float* %{output.arr_name}_{id}", file=OLD_FILE)
 
     print("ret void", file=OLD_FILE)
     print("}", file=OLD_FILE)
 
 
+def Forward(forward, max_range):
+    Args = get_default_args(forward)
+    LLVMForward(Args["_arg0"], Args["_arg1"], forward, [], max_range=max_range)
+
+
+def get_ssas_from_ir_line(line):
+    line = re.sub(r", align \d+", "", line)
+    idents = [f for f, _ in re.findall(r"(%[\d|a-z|_]*|([0-9]*[.])+[0-9]+)", line)]
+    if not idents:
+        print(line)
+        return None, None
+
+    if (
+        "fmul" in line
+        or "fadd" in line
+        or "fcmp" in line
+        or "fsub" in line
+        or "fdiv" in line
+    ):
+        assign, *deps = idents
+    elif "store" in line:
+        dep, assign = idents
+        deps = [dep]
+    elif "expf" in line or "relu" in line:
+        assign, *deps = idents
+    elif "fptrunc" in line:
+        assign, *deps = idents
+    elif "define void @forward" in line:
+        inputs = [
+            f.replace("float ", "")
+            for f, _ in re.findall(r"(float (%[\d|a-z|_]*))", line)
+        ]
+        outputs = [
+            f.replace("float* ", "")
+            for f, _ in re.findall(r"(float\* (%[\d|a-z|_]*))", line)
+        ]
+        return inputs, outputs
+    else:
+        raise Exception(line)
+
+    return assign, deps
+
+
+def crawl_graph(fp):
+    lines = open(fp).readlines()
+    G = nx.DiGraph()
+    inputs = None
+    outputs = None
+    for line in lines:
+        assign, deps = get_ssas_from_ir_line(line)
+        if "define" in line:
+            inputs = assign
+            outputs = deps
+            for assig in assign:
+                G.add_node(assig)
+            for dep in deps:
+                G.add_node(dep)
+        else:
+            if assign is not None:
+                if assign not in G.nodes:
+                    G.add_node(assign)
+                for dep in deps:
+                    if dep not in G.nodes:
+                        G.add_node(dep)
+                    G.add_edge(dep, assign)
+    for inp in inputs:
+        for outp in outputs:
+            try:
+                paths = nx.all_simple_paths(G, inp, outp)
+                for path in paths:
+                    print("path", inp, outp, len(path))
+            except nx.exception.NetworkXNoPath:
+                print("no path", inp, outp)
+                continue
+
+
+if __name__ == "__main__":
+    fp = sys.argv[1]
+    crawl_graph(fp)
 
 # declare void @_ssdm_op_SpecResource(...)
 #
