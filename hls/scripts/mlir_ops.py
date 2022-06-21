536bbe297f65db915df8982fda3e997b0115a371
max <maksim.levental@gmail.com>
2022-06-21 17:59:04 -0500

    incorporate val sem copy between conv layers

--- hls/scripts/mlir_ops.py
+++ hls/scripts/mlir_ops.py
@@ -397,7 +397,6 @@ class RemoveIfExp(ast.NodeTransformer):
     def visit_AugAssign(self, node):
         return node
 
-
 class HoistGlobals(ast.NodeTransformer):
     def visit_FunctionDef(self, node):
         if node.name == "forward":
@@ -407,6 +406,59 @@ class HoistGlobals(ast.NodeTransformer):
                 node.args.args.append(a.targets[0].id)
                 node.args.defaults.append(a.value)
         return node
+
+
+class RemoveValSemCopies(ast.NodeTransformer):
+    def visit_FunctionDef(self, node):
+        if node.name == "forward":
+            arg_names = [arg.arg if isinstance(arg, ast.arg) else arg for arg in node.args.args]
+            all_copies = [(i, b) for i, b in enumerate(node.body) if isinstance(b, _ast.FunctionDef) and len(b.body) == 2]
+            for i, copy in reversed(all_copies):
+                src = copy.body[0].value.value
+                if src.id in arg_names:
+                    continue
+                dst = copy.body[1].targets[0].value
+                assert isinstance(node.body[i+1].value, Call) and node.body[i+1].value.func.id == "ParFor"
+                node.body[i] = ast.Expr(value=Call(func=Name(id="Copy"), args=[dst, src], keywords=[keyword(arg="valsem", value=_ast.Constant(kind=None, value=True))]))
+                del node.body[i+1]
+        return node
+
+
+def parse_range_call(rang):
+    lo, hi, step = [r.value for r in rang.args]
+    return hi
+
+
+
+class SetMaxRange(ast.NodeTransformer):
+    def __init__(self, max_range):
+        super(SetMaxRange, self).__init__()
+        self.max_range = max_range
+
+    def visit_Call(self, node: Call) -> Any:
+        call_str = astor.code_gen.to_source(node)
+        if "Forward" in call_str:
+            node.keywords.append(
+                keyword(arg="max_range", value=Str([i for i in self.max_range]))
+            )
+        self.generic_visit(node)
+        return node
+
+
+def transform_forward_py(fp):
+    new_tree = astor.parse_file(fp)
+    new_tree = HoistGlobals().visit(new_tree)
+    new_tree = RemoveValSemCopies().visit(new_tree)
+    new_tree = RemoveIfExp().visit(new_tree)
+    with open(f"{fp.replace('forward', 'forward_rewritten')}", "w") as f:
+        f.write(astor.code_gen.to_source(new_tree))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fp")
+    args = parser.parse_args()
+    transform_forward_py(args.fp)
 
 
 # def unroll_loops():
@@ -425,64 +477,3 @@ class HoistGlobals(ast.NodeTransformer):
 #     open("new_new_body.py", "w").write(to_source(new_new_body))
 #     fun_tree = fun_tree.with_deep_changes(fun_tree, body=new_new_body)
 #     open("unrolled.py", "w").write(to_source(fun_tree))
-
-
-def parse_range_call(rang):
-    lo, hi, step = [r.value for r in rang.args]
-    return hi
-
-
-# class FindMaxRange(ast.NodeTransformer):
-#     max_range = None
-#
-#     def visit_Call(self, node: Call) -> Any:
-#         call_str = astor.code_gen.to_source(node)
-#         if "ParFor" in call_str:
-#             ranges = []
-#             for range_call in node.keywords[0].value.elts:
-#                 ranges.append(parse_range_call(range_call))
-#             max_range = max(list(itertools.product(*ranges)))
-#             if self.max_range is None:
-#                 self.max_range = max_range
-#             self.max_range = max(self.max_range, max_range)
-#         self.generic_visit(node)
-#         return node
-
-
-class SetMaxRange(ast.NodeTransformer):
-    def __init__(self, max_range):
-        super(SetMaxRange, self).__init__()
-        self.max_range = max_range
-
-    def visit_Call(self, node: Call) -> Any:
-        call_str = astor.code_gen.to_source(node)
-        if "Forward" in call_str:
-            node.keywords.append(
-                keyword(arg="max_range", value=Str([i for i in self.max_range]))
-            )
-        self.generic_visit(node)
-        return node
-
-
-def transform_forward_py(fp, macs=False):
-    new_tree = astor.parse_file(fp)
-    # new_tree = RemoveMAC().visit(new_tree)
-    new_tree = HoistGlobals().visit(new_tree)
-
-    # new_tree = RemoveMulAdd().visit(new_tree)
-    # if macs:
-    #     new_tree = RemoveMulAddAndSubscript().visit(new_tree)
-
-    # new_tree = SetMaxRange(max_range).visit(new_tree)
-    # new_tree = RemoveMul().visit(new_tree)
-    new_tree = RemoveIfExp().visit(new_tree)
-    with open(f"{fp.replace('forward', 'forward_rewritten')}", "w") as f:
-        f.write(astor.code_gen.to_source(new_tree))
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("fp")
-    parser.add_argument("--macs", action='store_true', default=False)
-    args = parser.parse_args()
-    transform_forward_py(args.fp, args.macs)
